8e01f3c5a8de1cdb88ece71904879e4d38f4da67
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2024-03-21 18:08:40 +0530

    created about component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from '../src/components/header/Header';
 import Home from './components/home/Home';
+import About from './components/about/About';
 function App() {
   return (
  <>
  <Header/>
  <main className='main'>
  <Home/>
+ <About/>
  </main>
  
  </>
---
+++ src/components/about/Info.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className="about_info grid">
+        <div className="about_box">
+            <i class="bx bx-award about_icon"></i>
+            <h3 className="about_title">Experience</h3>
+                <span className="about_subtitle">
+                2 years Working
+                </span>           
+        </div>
+        <div className="about_box">
+            <i class="bx bx-briefcase-alt about_icon"></i>
+            <h3 className="about_title">Completed</h3>
+                <span className="about_subtitle">
+                20 + projects
+                </span>           
+        </div>
+        <div className="about_box">
+            <i class="bx bx-support about_icon"></i>
+            <h3 className="about_title">Support</h3>
+                <span className="about_subtitle">
+                    Online 24/7
+                </span>           
+        </div>
+        
+    </div>
+  )
+}
+
+export default Info;
